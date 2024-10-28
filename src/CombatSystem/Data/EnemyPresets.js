c0638ea3ea8605892f604e5922f3c79268f87198
JaviFueyo <jfueyo@ucm.es>
2024-10-28 18:38:53 +0100

    Create EnemyPresets.js

---
+++ src/CombatSystem/Data/EnemyPresets.js
@@ -0,0 +1,45 @@
+export const EnemyPresets = 
+{
+    presets :
+    [
+        {
+            name: "Oxy",
+            damage: 10,
+            health: 100,
+            type: {horny: {name:"horny", str: "depression"}},
+            luck: 0.5,
+            image: "Javi",
+            damageSound: "oioioi"
+        },
+
+        {
+            name: "Emilio",
+            damage: 10,
+            health: 100,
+            type: {wrath: {name:"wrath", str: "anxiety"}},
+            luck: 0.5,
+            image: "Fueyo",
+            damageSound: "oioioi"
+        },
+
+        {
+            name: "Arturo",
+            damage: 10,
+            health: 100,
+            type: {depresion: {name:"depresion", str: "wrath"}},
+            luck: 0.5,
+            image: "Mika",
+            damageSound: "oioioi"
+        },
+
+        {
+            name: "Paco",
+            damage: 10,
+            health: 100,
+            type: {anxiety: {name:"anxiety", str: "horny"}},
+            luck: 0.5,
+            image: "Muxu",
+            damageSound: "oioioi"
+        }
+    ]
+}
