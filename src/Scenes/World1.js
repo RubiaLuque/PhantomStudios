d985266e2f2f82a79070167a5fe330073c3190a6
Lukatis16 <mika@ucm.es>
2024-10-31 00:56:10 +0100

    Enemigos en tilemap
    Ahora aparecen enemigos randomizados en el tilemap

--- src/Scenes/World1.js
+++ src/Scenes/World1.js
@@ -9,8 +9,6 @@ let pos = {x: 0, y: 0};
 let sceneAdded = false;
 let defeatedEnemiesIds = [];
 
-let Player
-
 const Type = {
     horny : {name:'horny', str: 'depression'},
     anxiety: {name:'anxiety', str: 'horny'},
@@ -44,6 +42,7 @@ export default class World1 extends Phaser.Scene
 
     create()
     {
+
         this.tileMap = this.make.tilemap({
             key: 'TestTileMap'
         })
@@ -53,17 +52,20 @@ export default class World1 extends Phaser.Scene
         this.collidables = this.tileMap.createLayer('Plataformas', set)
         this.collidables.setCollision(2);
 
-        this.player = this.tileMap.createFromObjects("Entidades", {name: 'Player', classType: player, key: 'Player'})
+        this.player = this.tileMap.createFromObjects("Entidades", {name: 'Player', classType: player, key: 'Main_Team'})[0] //key sirve para indicar que image carga
 
-        this.physics.add.collider(this.player[0], this.collidables)
+        this.physics.add.collider(this.player, this.collidables)
 
-        /*this.enemies = [];
-        for(let i = 0; i < 4; i++)
-        {
-            if(defeatedEnemiesIds.includes(i)) continue;
-
+        this.enemies = this.tileMap.createFromObjects("Entidades", {name: 'Enemy', key: EnemyPresets.presets[Math.floor(Math.random() * EnemyPresets.presets.length)][0]}); //Buscar forma de que este sprite sea aleatorio en cada miembro
+        console.log(this.enemies)
+        let enemyIndex = 0;
+        this.enemies.forEach(enemy =>{
+            
+            enemy.id = enemyIndex;
+            console.log(enemy.id)
+            if(defeatedEnemiesIds.includes(enemy.id)) this.enemies[enemyIndex].destroy()
+            else{
             let enemyPreset = EnemyPresets.presets[Math.floor(Math.random() * EnemyPresets.presets.length)];
-            let enemyImage = enemyPreset[0];
             let enemies = [];
 
             enemyPreset.forEach(enemy => {
@@ -71,13 +73,11 @@ export default class World1 extends Phaser.Scene
                 enemies.push(Entity.TranslateEntity(MainTeam.enemies[enemy], this.scene.scene));
             })
 
-            let enemy = new Phaser.GameObjects.Image(this, 125 * (i + 1), 125 * (i + 1), enemyImage);
-            this.add.existing(enemy);
             enemy.scale = 0.2;
             enemy.team = enemies;
-            enemy.id = i;
-            this.enemies.push(enemy);
-        }*/
+        }
+        enemyIndex++;
+        })
 
         if(!sceneAdded)
         {
@@ -85,20 +85,20 @@ export default class World1 extends Phaser.Scene
             this.scene.add('WinScene', WinScene);
             sceneAdded = true;
         }
-        this.cameras.main.startFollow(this.player[0], true, 0.05, 0.05);
+        this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
     }
 
     update()
     {
-        this.player[0].preUpdate()
+        this.player.preUpdate()
 
-        /*this.enemies.forEach(enemy => {
-            if(Phaser.Geom.Intersects.RectangleToRectangle(Player.getBounds(), enemy.getBounds()))
+        this.enemies.forEach(enemy => {
+            if(!defeatedEnemiesIds.includes(enemy.id) && Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemy.getBounds()))
             {
-                this.scene.start('combat', {team1: Player.team, team2: enemy.team, 
-                    lastPlayerPosition: {x: Player.x, y: Player.y}, enemyId: enemy.id});
+                this.scene.start('combat', {team1: this.player.team, team2: enemy.team, 
+                    lastPlayerPosition: {x: this.player.x, y: this.player.y}, enemyId: enemy.id});
             }
-        });*/
+        });
 
     }
 }
