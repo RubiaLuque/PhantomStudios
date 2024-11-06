8bbf8431973490443d6a6d74b34a337323350e8d
JaviFueyo <jfueyo@ucm.es>
2024-11-06 17:29:26 +0100

    Macho que había otras dos por ahí (SoyFueyo)

--- src/Scenes/CombatScene.js
+++ src/Scenes/CombatScene.js
@@ -167,14 +167,14 @@ export default class CombatScene extends Phaser.Scene {
         onPhaseChange = new Phaser.Events.EventEmitter();
 
         //Evento que se emite cuando se selecciona el ataque o la magia
-        onPhaseChange.on('combat', function(){
+        onPhaseChange.on('combat', ()=>{
             team2.entities.forEach(element => { element.sprite.setInteractive(); });
             phase = 'combat';
         });
 
         //Evento que se emite en cuanto se ataca a un enemigo despues de seleccionar el ataque o la magia
         //Cambiamos de personaje y si ya no hay mas personajes cambiamos de turno
-        onPhaseChange.on('next', function(){
+        onPhaseChange.on('next', ()=>{
                 currentCharacter++;
                 selectedCharacter = team1.GetCharacter(currentCharacter);
 
