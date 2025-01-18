48d9fe2c01831b2649e1cc6e9602d98d529385f6
Mika <mika@ucm.es>
2025-01-18 20:52:03 +0100

    mejor

--- src/CombatSystem/FloatingText.js
+++ src/CombatSystem/FloatingText.js
@@ -9,6 +9,7 @@ export default class FloatingText extends Phaser.GameObjects.Text
         this.setDepth(2);
         this.setScale(0.5);
         this.alpha = 0;
+        this.setFont('bold 60px Arial')
         this.setStroke('#FFF', 4)
     }
 
